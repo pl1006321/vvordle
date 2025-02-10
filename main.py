6eb8f902414ec4e6daef3579f91cd8421126b4bf
mandy wu <149692472+pl1006321@users.noreply.github.com>
2025-02-09 20:53:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ def import_words():
         words = file.read()    
     return words.split()
 
+def check_input(input):
+    if not input.isalpha() or len(input) != 5: 
+        return False
+    return True
+
+def print_lets(used):
+    for x in list(string.ascii_uppercase):
+        if x in used:
+            if used[x] == 'GREEN':
+                print(Fore.GREEN + x, end=' ')
+            elif used[x] == 'YELLOW':
+                print(Fore.YELLOW + x, end=' ')
+            else:
+                print(Fore.BLACK + x, end='')
+        else:
+            print(Fore.RESET + x, end=' ')
+    print('\n')
+
+
 words = import_words() 
 unused_lets = list(string.ascii_uppercase)
-used_lets = []
+used_lets = {'A':'GREEN', 'C':'YELLOW', 'Z':'BLACK'} # testing
+print_lets(used_lets)
